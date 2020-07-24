8f4da88e4d7340778f577a24f896a2a15adc88d9
francicothiagoufc <franciscothiago@alu.ufc.br>
2020-07-24 07:30:46 -0300

    Francisco Thiago 24/7: Criado a classe de teste da janela

---
+++ teste/View/TJanela.py
@@ -0,0 +1,60 @@
+class TJanela:
+
+	def testAdd():
+
+		app = AppContext()
+        interface = Interface(app.contextdelivery.contextos, app,breakFlag = True)
+
+        arquivo = "teste.c"
+        
+        interface.open(arquivo)
+
+        Interface.janelas[0].format.addChar('T')
+        Interface.janelas[0].format.addChar('e')
+        Interface.janelas[0].format.addChar('s')
+        Interface.janelas[0].format.addChar('T')
+        Interface.janelas[0].format.addChar('e')
+
+        assert interface.janelas[0].contexto.arquivo.conteudo[0] == list("Teste")
+
+
+    def testRemove:
+
+    	app = AppContext()
+        interface = Interface(app.contextdelivery.contextos, app,breakFlag = True)
+
+        arquivo = "teste.c"
+        
+        interface.open(arquivo)
+
+        Interface.janelas[0].format.addChar('T')
+        Interface.janelas[0].format.addChar('e')
+        Interface.janelas[0].format.addChar('s')
+        Interface.janelas[0].format.addChar('T')
+        Interface.janelas[0].format.addChar('e')
+
+        Interface.janelas[0].format.removeChar()
+        Interface.janelas[0].format.removeChar()
+        Interface.janelas[0].format.removeChar()
+        
+        assert interface.janelas[0].contexto.arquivo.conteudo[0] == list("Te")
+
+    
+    def testLines:
+
+    	app = AppContext()
+        interface = Interface(app.contextdelivery.contextos, app,breakFlag = True)
+
+        arquivo = "teste.c"
+        
+        interface.open(arquivo)
+
+        Interface.janelas[0].format.addLine()
+        Interface.janelas[0].format.addLine()
+        Interface.janelas[0].format.addLine()
+        Interface.janelas[0].format.addLine()
+        Interface.janelas[0].format.addLine()
+
+        
+        assert len(interface.janelas[0].contexto.arquivo.conteudo) == 6
+
