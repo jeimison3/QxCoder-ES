4185eba05703b60de530c3788ff1b9a99938ec2e
francicothiagoufc <franciscothiago@alu.ufc.br>
2020-07-24 08:12:57 -0300

    Francisco Thiago 24/7: Criado a classe de teste da janela

--- teste/View/TJanela.py
+++ teste/View/TJanela.py
@@ -62,3 +62,38 @@ class TJanela:
         
         assert len(interface.janelas[0].contexto.arquivo.conteudo) == 6
 
+    def testPonteiro(self):
+
+        app = AppContext()
+        interface = Interface(app.contextdelivery.contextos, app,breakFlag = True)
+
+        arquivo = "teste.c"
+        
+        interface.open(arquivo)
+
+        interface.janelas[0].format.addChar('T')
+        interface.janelas[0].format.addChar('e')
+        interface.janelas[0].format.addChar('s')
+        interface.janelas[0].format.addChar('t')
+        interface.janelas[0].format.addChar('e')
+
+        assert interface.janelas[0].ponteiro[1] = 5
+
+        interface.janelas[0].format.removeChar()
+        interface.janelas[0].format.removeChar()
+        interface.janelas[0].format.removeChar()
+
+        assert interface.janelas[0].ponteiro[1] = 2
+
+        interface.janelas[0].format.addLine()
+        interface.janelas[0].format.addLine()
+        interface.janelas[0].format.addLine()
+        interface.janelas[0].format.addLine()
+        interface.janelas[0].format.addLine()
+
+        assert interface.janelas[0].ponteiro[0] = 4
+
+        
+
+
+
